75987392222c973ebfa8b8f5375f912497ba4843
Kathiravan46 <Kathiravan4046@gmail.com>
2025-11-03 11:34:03 +0530

    new commit

--- obj/Debug/net9.0/Handson.AssemblyInfo.cs
+++ obj/Debug/net9.0/Handson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc302941c886cdb85c2a50d246f44c19b03aa89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398f57cb56530a5efdf8333570a3894ac959b7f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
